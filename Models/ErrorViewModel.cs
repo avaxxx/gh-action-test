3f632a1f2e032f86e6660458009a051bbd7ba41b
Pavol Decky <pavol.decky@quadrotech-it.com>
2021-01-30 10:27:46 +0100

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace gh_action_test.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
